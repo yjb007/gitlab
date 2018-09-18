16f5c0343df35ca9122a2abc27e7afd10fb4749e
yjb007 <14339989@qq.com>
2018-09-18 18:34:24 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -15,6 +15,8 @@ docker service create \
     --network my-network \
     --replicas 1 \
     --publish "mode=host,published=22,target=22" \
+    --publish "mode=host,published=80,target=80" \
+    --publish "mode=host,published=443,target=443" \
     --mount type=bind,src=/srv/volume/git/config,dst=/etc/gitlab \
     --mount type=bind,src=/srv/volume/git/logs,dst=/var/log/gitlab \
     --mount type=bind,src=/srv/volume/git/data,dst=/var/opt/gitlab \
